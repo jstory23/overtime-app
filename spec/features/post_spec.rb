27bfa92e2a2c6f382c20607533edf95beaa2c126
jstory23 <jordanstory23@gmail.com>
2017-02-15 11:28:44 -0700

    Implemented tests and page redirection for permission structure

--- spec/features/post_spec.rb
+++ spec/features/post_spec.rb
@@ -10,6 +10,7 @@ describe 'navigate' do
     before do
       visit posts_path
     end
+
     it 'can be reached successfully' do
       expect(page.status_code).to eq(200)
     end
@@ -62,7 +63,7 @@ describe 'navigate' do
       expect(page).to have_content("Some rationale")
     end
 
-    it 'will have a user associated with it' do
+    it 'will have a user associated it' do
       fill_in 'post[date]', with: Date.today
       fill_in 'post[rationale]', with: "User Association"
       click_on "Save"
@@ -71,29 +72,31 @@ describe 'navigate' do
     end
   end
 
-
   describe 'edit' do
     before do
-    @post = FactoryGirl.create(:post)
-    end
-
-    it 'can be reached by clicking edit on index page' do
-
-      visit posts_path
-
-      click_link("edit_#{@post.id}")
-      expect(page.status_code).to eq(200)
+      @edit_user = User.create(first_name: "asdf", last_name: "asdf", email: "asdfasdf@asdf.com", password: "asdfasdf", password_confirmation: "asdfasdf")
+      login_as(@edit_user, :scope => :user)
+      @edit_post = Post.create(date: Date.today, rationale: "asdf", user_id: @edit_user.id)
     end
 
     it 'can be edited' do
-      visit edit_post_path(@post)
+      visit edit_post_path(@edit_post)
 
       fill_in 'post[date]', with: Date.today
       fill_in 'post[rationale]', with: "Edited content"
       click_on "Save"
 
       expect(page).to have_content("Edited content")
+    end
 
+    it 'cannot be edited by a non authorized user' do
+      logout(:user)
+      non_authorized_user = FactoryGirl.create(:non_authorized_user)
+      login_as(non_authorized_user, :scope => :user)
+
+      visit edit_post_path(@edit_post)
+
+      expect(current_path).to eq(root_path)
     end
   end
 end
