3b4a981bee780cde3bd7f484cd4212258faaa0cc
jstory23 <jordanstory23@gmail.com>
2017-02-23 15:47:42 -0700

    Built rake task for sending emails.

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,4 +1,4 @@
-class Post < ApplicationRecord
+class Post < ActiveRecord::Base
 	enum status: { submitted: 0, approved: 1, rejected: 2 }
 	belongs_to :user
 	validates_presence_of :date, :rationale, :overtime_request
--- spec/features/approval_workflow_spec.rb
+++ spec/features/approval_workflow_spec.rb
@@ -1,16 +1,15 @@
 require 'rails_helper'
 
 describe 'navigate' do
-  before do
-    @admin_user = FactoryGirl.create(:admin_user)
-    login_as(@admin_user, :scope => :user)
-  end
+	before do
+		@admin_user = FactoryGirl.create(:admin_user)
+		login_as(@admin_user, :scope => :user)
+	end
 
-  describe 'edit' do
+	describe 'edit' do
     before do
       @post = FactoryGirl.create(:post)
       visit edit_post_path(@post)
-
     end
 
     it 'has a status that can be edited on the form by an admin' do
@@ -30,7 +29,7 @@ describe 'navigate' do
       expect(page).to_not have_content('Approved')
     end
 
-    it 'should not be editable by the post creator if the status is approved' do
+    it 'should not be editable by the post creator if status is approved' do
       logout(:user)
       user = FactoryGirl.create(:user)
       login_as(user, :scope => :user)
@@ -41,5 +40,5 @@ describe 'navigate' do
 
       expect(current_path).to eq(root_path)
     end
-  end
+	end
 end
