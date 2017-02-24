679da47496677529d6a35e318605c9f06a19023f
jstory23 <jordanstory23@gmail.com>
2017-02-23 20:34:03 -0700

    updated phone number for twilio app

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,31 +1,31 @@
-@user = User.create(email: "test@test.com",
+@employee = Employee.create(email: "test@test.com",
                     password: "asdfasdf",
                     password_confirmation: "asdfasdf",
                     first_name: "Jon",
                     last_name: "Snow",
-                    phone: "4322386131")
+                    phone: "3858316099")
 
-puts "1 User created"
+puts "1 employee created"
 
 AdminUser.create(email: "admin@test.com",
                   password: "asdfasdf",
                   password_confirmation: "asdfasdf",
                   first_name: "Admin",
-                  last_name: "User",
-                  phone: "4322386131")
+                  last_name: "employee",
+                  phone: "3858316099")
 
-puts "1 Admin User created"
+puts "1 Admin user created"
 
-  AuditLog.create!(user_id: @user.id, status: 0, start_date: (Date.today - 6.days))
-  AuditLog.create!(user_id: @user.id, status: 0, start_date: (Date.today - 13.days))
-  AuditLog.create!(user_id: @user.id, status: 0, start_date: (Date.today - 20.days))
+  AuditLog.create!(user_id: @employee.id, status: 0, start_date: (Date.today - 6.days))
+  AuditLog.create!(user_id: @employee.id, status: 0, start_date: (Date.today - 13.days))
+  AuditLog.create!(user_id: @employee.id, status: 0, start_date: (Date.today - 20.days))
 
 puts "3 audit logs have been create."
 
 100.times do |post|
   Post.create!(date: Date.today, rationale: "#{post} rationale content At vero eos et accusamus et iusto odio dignissimos ducimus
   qui blanditiis praesentium voluptatum deleniti atque corrupti quos dolores et quas molestias excepturi sint occaecati cupiditate non provident,
-  similique sunt in culpa qui officia deserunt mollitia animi, id est laborum et dolorum fuga.", user_id: @user.id, overtime_request: 2.5)
+  similique sunt in culpa qui officia deserunt mollitia animi, id est laborum et dolorum fuga.", user_id: @employee.id, overtime_request: 2.5)
 end
 
 puts "100 posts have been create."
