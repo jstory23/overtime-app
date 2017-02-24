c0a5dfa33f0698238f0d7a28d8531136a3ee66ed
jstory23 <jordanstory23@gmail.com>
2017-02-23 21:17:56 -0700

    Integrated sms rake task to include audit log creation

--- config/initializers/setup_mail.rb
+++ config/initializers/setup_mail.rb
@@ -1,9 +1,9 @@
-ActionMailer::Base.smtp_settings = {
-    :port =>      ENV['SPARKPOST_SMTP_PORT'],
-    :address =>   ENV['SPARKPOST_SMTP_HOST'],
-    :user_name => ENV['SPARKPOST_SMTP_USERNAME'],
-    :password =>  ENV['SPARKPOST_SMTP_PASSWORD'],
-    :authentication => :login,
-    :enable_starttls_auto => true
-}
-ActionMailer::Base.deliver_method = :smtp
+# ActionMailer::Base.smtp_settings = {
+#     :port =>      ENV['SPARKPOST_SMTP_PORT'],
+#     :address =>   ENV['SPARKPOST_SMTP_HOST'],
+#     :user_name => ENV['SPARKPOST_SMTP_USERNAME'],
+#     :password =>  ENV['SPARKPOST_SMTP_PASSWORD'],
+#     :authentication => :login,
+#     :enable_starttls_auto => true
+# }
+# ActionMailer::Base.deliver_method = :smtp
